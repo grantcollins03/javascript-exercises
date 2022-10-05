d899bdafb51e7336ec90df7f2c5679f267f33099
Grant Collins <grantcollins03@gmail.com>
2022-10-05 17:15:16 -0400

    Completed 08 calculator exercises

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,49 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+  return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+	let sum = 0;
+  if (array.length === 0) {
+    sum = 0;
+  } else {
+    for (i = 0; i < array.length; i++) {
+      sum += array[i];
+    }
+  }
+  return sum;
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  let product = 1;
+  if (array.length === 0) {
+    console.log("The given array is empty.");
+  } else {
+    for (i = 0; i < array.length; i++) {
+      product *= array[i];
+    }
+  }
+  return product;
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	return a ** b;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+  let product = 1;
+	if (num === 0) {
+    product = 1;
+  } else {
+    for (i = 1; i <= num; i++) {
+      product *= i;
+    }
+  }
+  return product;
 };
 
 // Do not edit below this line
