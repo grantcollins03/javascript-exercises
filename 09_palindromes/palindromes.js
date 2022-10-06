f8435f726a43e9c4b93520885a7e48968c5fa74f
Grant Collins <grantcollins03@gmail.com>
2022-10-06 19:59:17 -0400

    Solved 09_palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,12 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    let array = str
+        .toLowerCase()
+        .replace(/[.,/#!$%^&*;:{}=-_`~()<> ]/g, '')
+        .split('');
+    let reversed = [...array].reverse();
+    return (array.join('') === reversed.join(''))
 };
+palindromes("racecar");
 
 // Do not edit below this line
 module.exports = palindromes;
